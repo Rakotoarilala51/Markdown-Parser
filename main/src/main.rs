9df88ba6edfb1872b93970e155bb5c49e7cfde89
Rakotoarilala51 <maitreaina51@gmail.com>
2025-07-04 21:04:06 +0300

    setting command line argument parser

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,10 @@
+use clap::{clap_app, crate_version};
 fn main() {
+    let _clap = clap_app!(main=> 
+    (version:crate_version!())
+    (author:"The Ramaitre")
+    (about:"render markdown as you like")
+    (@arg input: +required "Sets the input file"))
+    .get_matches();
     println!("Hello, world!");
 }
